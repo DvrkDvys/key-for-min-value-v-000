2325dd3f2e29a517679e074ab688202b58b51f15
Jordan Harris <jharris@wework.com>
2018-07-10 20:46:05 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,8 +3,8 @@
 require 'pry'
 
 def key_for_min_value(hash)
-  # lowest_key = nil
-  # lowest_value = Float::INFINITY
+  lowest_key = nil
+  lowest_value = Float::INFINITY
   hash.each do |k, v|
     if v < lowest_value
       lowest_value = v
