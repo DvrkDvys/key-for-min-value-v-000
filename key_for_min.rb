2eb8e8b2cd80424da206d6a11816f307c509fe91
Jordan Harris <jharris@wework.com>
2018-07-10 20:40:45 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,15 +3,7 @@
 require 'pry'
 
 def key_for_min_value(name_hash)
- i = name_hash.sort
-  binding.pry
-    return name_hash
+  i = name_hash.sort
+  i[0] do |k, v|    
+    return k
 end
-
-# (hash.keys).each {|i| when (hash[i] <  hash[i + 1]) puts 
-
-
-
-# hash = {:blake => 500, :ashley => 2, :adam => 1}
-
-#     expect(hash).to_not receive(:keys)
